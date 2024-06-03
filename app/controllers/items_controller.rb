92cc52bc1759dcdc46bd7694f67aaab3d06fb32b
renta0525 <renzu525@gmail.com>
2024-06-04 04:13:04 +0900

    新規登録機能を実装する（バリデーション、エラーメッセージの設定）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,8 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def destroy
+    redirect_to root_path
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: ' is invalid. Input full-width characters' } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: ' is invalid. Input full-width katakana characters' } do
+    validates :first_name_read
+    validates :last_name_read
+  end
+  validates :birthday, presence: true
 end
