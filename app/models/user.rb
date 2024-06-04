e3552454866a0933c923e8d91802cd15ff23cd86
renta0525 <renzu525@gmail.com>
2024-06-05 01:30:18 +0900

    ログイン、ログアウト機能とモデルの単体テストを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: ' is invalid. Input full-width characters' } do
     validates :first_name
     validates :last_name
