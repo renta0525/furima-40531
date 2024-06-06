966d8077ac4b1de2c77618bb7291ea5e0eaf0dc2
renta0525 <renzu525@gmail.com>
2024-06-07 00:43:47 +0900

    Active Storageの導入

--- Gemfile
+++ Gemfile
@@ -86,3 +86,6 @@ end
 
 
 gem 'devise'
+
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,3 +2,9 @@ class ItemsController < ApplicationController
   def index
   end
 end
+
+private
+
+def message_params
+  params.require(:item).permit(:image).merge(user_id: current_user.id)
+end
