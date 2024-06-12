f5bbf8c2fb95a1102974f0f3c92574ae44fae40e
renta0525 <renzu525@gmail.com>
2024-06-12 22:52:35 +0900

    ActiveHash,バリデーションを設定, モデルの単体テストコード,販売手数料や販売利益の計算,

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
 
 private
 
-def message_params
-  params.require(:item).permit(:image).merge(user_id: current_user.id)
+def item_params
+  params.require(:item).permit(:image, :item_name, :item_explanation, :category_id, :condition_id, :contribution_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
 end
