c17043345fde0f5113701975a9b27917a0e642c0
renta0525 <renzu525@gmail.com>
2024-06-25 18:37:15 +0900

    テーブル作成

--- db/migrate/20240625063305_create_addresses.rb
+++ db/migrate/20240625063305_create_addresses.rb
@@ -1,7 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[7.0]
   def change
     create_table :addresses do |t|
-
+      t.references :order,        null: false, foreign_key: true
+      t.string :post_cord,        null: false
+      t.integer :prefecture_id,   null: false
+      t.string :municipalities,   null: false
+      t.string :address,          null: false
+      t.string :building_name
+      t.string :phone_number,     null: false
       t.timestamps
     end
   end
