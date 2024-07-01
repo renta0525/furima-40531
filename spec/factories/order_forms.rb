0de730ed0f0217e39a9d1f3a148643bc0a9d7dbd
renta0525 <renzu525@gmail.com>
2024-07-01 19:45:13 +0900

    購入機能テストコード

---
+++ spec/factories/order_forms.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :order_form do
+    user_id                 {1}
+    item_id                 {1}
+    order_id                {1}
+    post_cord               {Faker::Number.leading_zero_number(digits: 3) + '-' + Faker::Number.leading_zero_number(digits: 4)}
+    prefecture_id           {Faker::Number.between(from: 2, to: 48)}
+    municipalities          {Faker::Address.city}
+    address                 {Faker::Address.street_address}
+    building_name           {Faker::Company.name}
+    phone_number            {Faker::Number.leading_zero_number(digits: 11)}
+    token                   {Faker::Alphanumeric.alphanumeric(number: 30)}
+  end
+end
